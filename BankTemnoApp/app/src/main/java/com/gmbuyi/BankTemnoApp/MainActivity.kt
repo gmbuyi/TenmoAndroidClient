b5b5ac881714f32cb518c6a34885a2d9556d2c94
gmbuyi <georgiembuyi@gmail.com>
2022-09-28 00:53:08 -0700

    Sing In Layout Created

---
+++ BankTemnoApp/app/src/main/java/com/gmbuyi/BankTemnoApp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.gmbuyi.BankTemnoApp
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.sign_in_layout)
+
+    }
+
+
+
+}
